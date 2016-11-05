8560b9e1a4a7d6a08acbf171b900547454d75031
root <localducks@gmail.com>
2016-11-05 19:26:52 +0300

    Initial commit

---
+++ app/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+import Panel from "../components/Panel.jsx";
+
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {parentState: "Kotlin"}
+
+     this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
+   handleUserInput(lang) {
+        this.setState({
+             parentState: lang
+        });
+    }
+
+
+
+  render(){
+     return (
+       <div>
+         <h1>Parent state {this.state.parentState}</h1>
+         <Panel onUserInput = {this.handleUserInput} parentState = {this.state.parentState} />
+        </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
---
+++ components/Panel.jsx
@@ -0,0 +1,31 @@
+/**
+ * Created by Pavlovens on 04.11.2016.
+ */
+"use strict"
+import React, { Component } from 'react';
+
+export default class Panel extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+}
+
+    handleChange() {
+        this.props.onUserInput(
+            this.refs.h1input.value,
+        );
+    }
+
+
+    render(){
+       return(
+            <div >
+              <input onChange={this.handleChange} type="text" ref = "h1input" value={this.props.parentState}/>
+            </div>
+        )
+    }
+
+
+}
